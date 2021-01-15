8b35cb11f2c353b5b8c30715f46bfba575378e0e
david <calhoun5321@gmail.com>
2021-01-14 16:19:00 -0800

    adds .take query

--- app/controllers/api/stars_controller.rb
+++ app/controllers/api/stars_controller.rb
@@ -1,8 +1,9 @@
 class Api::StarsController < ApplicationController
 
   def actor_action
-    @actor = Actor.find_by(id: 1)
+    @actor = Actor.take(2)
     render "stars.json.jb"
   end
 
 end
+# @actor = Actor.find_by(id: 1)
