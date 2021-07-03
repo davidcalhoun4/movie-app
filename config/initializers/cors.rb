f1555deb880d62e8cc336aab131427ca82426c7d
david <calhoun5321@gmail.com>
2021-07-03 13:53:32 -0700

    fixes cors.rb to allow queerflix.netlify.app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'localhost:3000' 'queerflix.netlify.app'
+    origins 'localhost:8080', 'localhost:3000', 'queerflix.netlify.app'
 
     resource '*',
       headers: :any,
